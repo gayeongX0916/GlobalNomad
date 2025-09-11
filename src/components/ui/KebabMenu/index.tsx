5c129b9203f89a925a7fc3862599c1657b3153a5
gayeongX0916 <gayeongx0916@gmail.com>
2025-09-11 17:13:11 +0900

    [feat/#55] 예약 내역 페이지 퍼블리싱

--- src/components/ui/KebabMenu/index.tsx
+++ src/components/ui/KebabMenu/index.tsx
@@ -43,8 +43,7 @@ export function KebabMenu() {
           src={MoreIcon}
           alt=""
           aria-hidden="true"
-          width={40}
-          height={40}
+          className="w-[32px] h-[32px] md:w-[40px] md:h-[40px]"
         />
       </button>
       {isOpen && (
