56e473982f98446c41a79ad215baf205ba92cc59
gayeongX0916 <gayeongx0916@gmail.com>
2025-09-18 19:40:30 +0900

    [feat/#71] utils 함수 수정

--- src/lib/utils/cookies.ts
+++ src/lib/utils/cookies.ts
@@ -1,52 +1,33 @@
 "use client";
 import { getCookie, setCookie, deleteCookie } from "cookies-next";
 
-interface TokenProps {
-  accessToken?: string;
-  refreshToken?: string;
-}
-
 const isProd = process.env.NODE_ENV === "production";
 
-const BASE_OPTS = {
+const BASE = {
   secure: isProd,
-  sameSite: "lax" as const,
+  sameSite: "strict" as const,
   path: "/",
 };
 
 const KEYS = {
-  access: "accessToken",
   refresh: "refreshToken",
 };
 
-export function setCookiesByToken({ accessToken, refreshToken }: TokenProps) {
-  if (accessToken) {
-    setCookie(KEYS.access, accessToken, {
-      ...BASE_OPTS,
-      httpOnly: false,
-      maxAge: 60 * 10,
-    });
-  }
-  if (refreshToken) {
-    setCookie(KEYS.refresh, refreshToken, {
-      ...BASE_OPTS,
-      httpOnly: false,
-      maxAge: 60 * 60 * 24 * 7,
-    });
-  }
+export function setRefreshCookie(
+  refreshToken: string,
+  maxAgeSec = 60 * 60 * 24 * 3
+) {
+  setCookie(KEYS.refresh, refreshToken, {
+    ...BASE,
+    httpOnly: false,
+    maxAge: maxAgeSec,
+  });
 }
 
-export function getTokensFromCookies() {
-  const accessToken = getCookie(KEYS.access);
-  const refreshToken = getCookie(KEYS.refresh);
-
-  return {
-    accessToken,
-    refreshToken,
-  };
+export function getRefreshFromCookie() {
+  return getCookie(KEYS.refresh) as string | undefined;
 }
 
-export function deleteTokensFromCookies() {
-  deleteCookie(KEYS.access);
-  deleteCookie(KEYS.refresh);
+export function deleteRefreshCookie() {
+  deleteCookie(KEYS.refresh, { path: "/" });
 }
