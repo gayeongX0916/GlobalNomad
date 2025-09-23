4e1a14dd7343c8bc0993ad3a55abbcae13e4f209
gayeongX0916 <gayeongx0916@gmail.com>
2025-09-23 18:17:55 +0900

    [feat/#78] api 설정

---
+++ src/lib/api/myreservations.ts
@@ -0,0 +1,49 @@
+import {
+  CreateMyReservationReviewsBody,
+  CreateMyReservationReviewsResponse,
+  MyReservationListBody,
+  MyReservationListResponse,
+  UpdateMyReservationBody,
+  UpdateMyReservationResponse,
+} from "../types/myreservations";
+import { basicAxios } from "./basicAxios";
+
+// 내 예약 리스트 조회
+export const getMyReservationList = async (
+  params: MyReservationListBody
+): Promise<MyReservationListResponse> => {
+  const { data } = await basicAxios.get<MyReservationListResponse>(
+    `/my-reservations?size=10`,
+    {
+      params: {
+        status: params.status,
+      },
+    }
+  );
+  return data;
+};
+
+// 내 예약 수정 (취소)
+export const patchMyReservation = async ({
+  reservationId,
+  status,
+}: UpdateMyReservationBody): Promise<UpdateMyReservationResponse> => {
+  const { data } = await basicAxios.patch<UpdateMyReservationResponse>(
+    `/my-reservations/${reservationId}`,
+    status
+  );
+  return data;
+};
+
+// 내 예약 리뷰 작성
+export const postMyReservationReviews = async ({
+  reservationId,
+  rating,
+  content,
+}: CreateMyReservationReviewsBody): Promise<CreateMyReservationReviewsResponse> => {
+  const { data } = await basicAxios.post<CreateMyReservationReviewsResponse>(
+    `/my-reservations/${reservationId}/review`,
+    { rating, content }
+  );
+  return data;
+};
