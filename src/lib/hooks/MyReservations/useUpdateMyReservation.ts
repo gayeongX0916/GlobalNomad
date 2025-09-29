f8172bf85a917beac5b1403639ea18cdb3057c77
gayeongX0916 <gayeongx0916@gmail.com>
2025-09-29 18:23:31 +0900

    [feat/#92] 리뷰 api 연결

--- src/lib/hooks/MyReservations/useUpdateMyReservation.ts
+++ src/lib/hooks/MyReservations/useUpdateMyReservation.ts
@@ -1,12 +1,17 @@
 import { patchMyReservation } from "@/lib/api/myReservations";
 import { extractErrorMessage } from "@/lib/utils/ErrorMessage";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-toastify";
 
 export function useUpdateMyReservation() {
+  const qc = useQueryClient();
+
   return useMutation({
     mutationFn: patchMyReservation,
-    onSuccess: () => toast.success("내 예약이 취소되었습니다."),
+    onSuccess: () => {
+      toast.success("내 예약이 취소되었습니다.");
+      qc.invalidateQueries({ queryKey: ["myreservation"] });
+    },
     onError: (error) => toast.error(extractErrorMessage(error)),
   });
 }
