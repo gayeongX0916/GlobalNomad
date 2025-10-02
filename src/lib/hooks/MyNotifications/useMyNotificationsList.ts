c80942034d7e5d8326f0bd8589588dd1d84f77ad
gayeongX0916 <gayeongx0916@gmail.com>
2025-10-02 17:16:30 +0900

    [feat/#100] 알림 useInfiniteQuery 사용

--- src/lib/hooks/MyNotifications/useMyNotificationsList.ts
+++ src/lib/hooks/MyNotifications/useMyNotificationsList.ts
@@ -1,13 +1,25 @@
 import { getMyNotifications } from "@/lib/api/myNotifications";
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
-export const QK_MY_NOTIS= ["my-notifications"] as const;
+export const QK_MY_NOTIS = ["my-notifications"] as const;
 
-export function useMyNotificationsList(enabled:boolean) {
-  return useQuery({
+type useMyNotificationsListProps = {
+  enabled?: boolean;
+  size?: number;
+};
+
+export function useMyNotificationsList({
+  enabled,
+  size,
+}: useMyNotificationsListProps) {
+  return useInfiniteQuery({
     queryKey: QK_MY_NOTIS,
-    queryFn: getMyNotifications,
     enabled,
-    refetchOnMount:"always"
+    queryFn: ({ pageParam }: { pageParam: number | null }) =>
+      getMyNotifications({ size, cursorId: pageParam }),
+    initialPageParam: null,
+    getNextPageParam: (nextPageParam) => nextPageParam.cursorId ?? null,
+    refetchInterval: 1000 * 60, 
+    refetchOnWindowFocus: false,
   });
 }
