57701e863dbb56fb5b882b055992674d232d17d6
gayeongX0916 <gayeongx0916@gmail.com>
2025-10-10 17:47:29 +0900

    [feat/#114] 수정 후 revalidateTag

--- src/lib/hooks/MyActivities/useUpdateMyActivities.ts
+++ src/lib/hooks/MyActivities/useUpdateMyActivities.ts
@@ -1,14 +1,29 @@
+import { revalidateActivityTag } from "@/components/activity/actions";
 import { patchMyActivities } from "@/lib/api/myActivities";
 import { UpdateMyActivitiesBody } from "@/lib/types/myActivities";
 import { extractErrorMessage } from "@/lib/utils/ErrorMessage";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
-export function useUpdateMyActivites(){
-    return useMutation({
-        mutationFn: ({ activityId, body }: { activityId: number; body: UpdateMyActivitiesBody }) =>
-            patchMyActivities(activityId, body),
-        onSuccess:()=>toast.success("체험이 수정되었습니다."),
-        onError:(error)=>toast.error(extractErrorMessage(error)),
-    } )
+export function useUpdateMyActivites() {
+  const router = useRouter();
+
+  return useMutation({
+    mutationFn: ({
+      activityId,
+      body,
+    }: {
+      activityId: number;
+      body: UpdateMyActivitiesBody;
+    }) => patchMyActivities(activityId, body),
+      onSuccess: async (updated) => {
+      await revalidateActivityTag(updated.id); 
+      router.push(`/activities/${updated.id}`);
+      router.refresh();                       
+      toast.success("체험이 수정되었습니다.");
+    },
+
+    onError: (error) => toast.error(extractErrorMessage(error)),
+  });
 }
