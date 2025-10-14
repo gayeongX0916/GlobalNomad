4c9876e1c7a90a44027b144f6984e5a49364e964
gayeongX0916 <gayeongx0916@gmail.com>
2025-10-14 15:19:59 +0900

    [feat] FilterSlide 재수정

--- src/components/ui/Button/CategoryButton.tsx
+++ src/components/ui/Button/CategoryButton.tsx
@@ -13,8 +13,10 @@ export function CategoryButton({
 }: CategoryButtonProps) {
   return (
     <button
-      className={`px-[10px] py-[8px] md:px-[25px] lg:px-[30px] md:py-[16px] rounded-[15px] border border-nomadBlack bg-white text-green-900 text-lg md:text-2lg cursor-pointer hover:bg-nomadBlack hover:text-white w-full whitespace-nowrap ${
-        active ? "bg-nomadBlack text-white" : "bg-white text-green-900"
+      className={`px-[10px] py-[8px] md:px-[25px] lg:px-[30px] md:py-[16px] rounded-[15px] border border-nomadBlack text-lg md:text-2lg cursor-pointer w-full whitespace-nowrap ${
+        active
+          ? "bg-nomadBlack text-white"
+          : "bg-white text-green-900 hover:bg-nomadBlack hover:text-white"
       }`}
       onClick={onClick}
     >
